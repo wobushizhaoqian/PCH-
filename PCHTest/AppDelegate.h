56612df64c9ebbcd1dc563bb370ec0bd26b7e04d
赵前 <zhaoqian@zhaoqiandemacbook-air.local>
2019-03-15 09:45:35 +0800

    Initial Commit

---
+++ PCHTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCHTest
+//
+//  Created by 赵前 on 2019/3/15.
+//  Copyright © 2019年 赵前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCHTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCHTest
+//
+//  Created by 赵前 on 2019/3/15.
+//  Copyright © 2019年 赵前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
