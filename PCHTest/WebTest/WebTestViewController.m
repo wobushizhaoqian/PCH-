ed095c735ce077082d2b4b33baffb4423e1ccb36
赵前 <zhaoqian@zhaoqiandeMacBook-Air.local>
2019-03-19 09:39:44 +0800

    add WKWebView demo

--- PCHTest/WebTest/WebTestViewController.m
+++ PCHTest/WebTest/WebTestViewController.m
@@ -9,7 +9,9 @@
 #import "WebTestViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface WebTestViewController ()<WKUIDelegate,WKNavigationDelegate>
+@interface WebTestViewController ()<WKUIDelegate,WKNavigationDelegate,WKScriptMessageHandler>
+
+@property (nonatomic, strong)WKWebView *webView;
 
 @end
 
@@ -17,37 +19,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    config.preferences = [[WKPreferences alloc] init];
-    config.preferences.minimumFontSize = 10;
-    config.preferences.javaScriptEnabled = YES;
-    config.preferences.javaScriptCanOpenWindowsAutomatically = NO;
-    config.userContentController = [[WKUserContentController alloc] init];
-    [config.userContentController addScriptMessageHandler:self name:@"alert"];
-    config.processPool = [[WKProcessPool alloc] init];
-    WKWebView * webView = [[WKWebView alloc]initWithFrame:self.view.bounds configuration:config];
-    webView.UIDelegate = self;
-    webView.navigationDelegate = self;
-    webView.allowsBackForwardNavigationGestures = YES;
-
- 
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"test.html" ofType:nil];
-    NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-    //加载本地html文件
-    [webView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
-    
-    [self.view addSubview:webView];
+    [self.view addSubview:self.webView];
     // Do any additional setup after loading the view.
 }
 
-- (void)addScriptMessageHandler:(id <WKScriptMessageHandler>)scriptMessageHandler name:(NSString *)name{
-    
+
+
+#pragma mark ------------------- WKNavigationDelegate -------------------
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
 }
+
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+}
+
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation {
+}
+
+
+
+
+#pragma mark ------------------- WKScriptMessageHandler -------------------
 
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
-    NSLog(@"123");
+    // 获取js点击
+    // message.body 为post过来的参数  id类型
+    NSLog(@"%@",[NSString stringWithFormat:@"%@",message.name]);
+    
+    //点击之后调用下js里面自己的方法
+    [self evaluateJSWithString:@"hello2()"];
+}
+#pragma mark ------------------- private method -------------------
+
+- (void)evaluateJSWithString:(NSString *)string{
+    [self.webView evaluateJavaScript:string completionHandler:^(id _Nullable respondse, NSError * _Nullable error) {
+        
+    }];
 }
 
+
+#pragma mark ------------------- lazy loading -------------------
+- (WKWebView *)webView{
+    if (!_webView) {
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc]init];
+        config.preferences = [WKPreferences new];
+        config.preferences.javaScriptEnabled = YES;
+        config.preferences.minimumFontSize = 40;
+        config.preferences.javaScriptCanOpenWindowsAutomatically = NO;
+        config.userContentController = [[WKUserContentController alloc]init];
+        [config.userContentController addScriptMessageHandler:self name:@"ScanAction"];
+        _webView = [[WKWebView alloc]initWithFrame:self.view.bounds configuration:config];
+        _webView.navigationDelegate = self;
+        _webView.UIDelegate = self;
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"test.html" ofType:nil];
+        NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+        //加载本地html文件
+        [_webView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
+    }
+    return _webView;
+}
 
 
 @end
